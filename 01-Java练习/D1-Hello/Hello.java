c3a62ede4dd2f3772588c470fc9da2be6200a713
霍健卓 <1243174864@qq.com>
2026-09-22 21:36:17 +0800

    Revert "改了一下文字"
    This reverts commit 16453441d6ce867c3335022b0eb15039cdd7551d.

--- 01-Java练习/D1-Hello/Hello.java
+++ 01-Java练习/D1-Hello/Hello.java
@@ -1,7 +1,7 @@
 public class Hello 
 {
     public static void main(String[] args) {
-        System.out.println("wogaihuaile！");
+        System.out.println("你好，光伏！");
         System.out.println("我是霍健卓，这是我的第一个 Java 程序。");
     }
 }
