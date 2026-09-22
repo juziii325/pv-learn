f3f353ac8982c5250ab2cf9dffe2d34516b3d2ff
霍健卓 <1243174864@qq.com>
2026-09-23 00:31:22 +0800

    主干上改了第6行

--- 01-Java练习/D1-Hello/Hello.java
+++ 01-Java练习/D1-Hello/Hello.java
@@ -3,6 +3,6 @@ public class Hello
     public static void main(String[] args) {
         System.out.println("你好，光伏！");
         System.out.println("我是霍健卓。");
-        System.out.println("我是霍健卓，这是我的第一个 Java 程序。");
+        System.out.println("主干上改的这句话。");
     }
 }
