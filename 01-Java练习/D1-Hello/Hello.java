baec2ef19190f4b0fdfc258e9c457b2b6066cc32
霍健卓 <1243174864@qq.com>
2026-09-23 00:59:01 +0800

    加了一行

--- 01-Java练习/D1-Hello/Hello.java
+++ 01-Java练习/D1-Hello/Hello.java
@@ -4,5 +4,6 @@ public class Hello
         System.out.println("你好，光伏！");
         System.out.println("我是霍健卓。");
         System.out.println("主干上改的这句话。");
+        System.out.println("这是我加的新行。");
     }
 }
