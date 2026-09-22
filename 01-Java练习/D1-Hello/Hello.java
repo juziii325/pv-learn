83ad287ec99c99d4b1ddd306d9cb8c0582f796af
霍健卓 <1243174864@qq.com>
2026-09-22 21:51:38 +0800

    新增一行自我介绍

--- 01-Java练习/D1-Hello/Hello.java
+++ 01-Java练习/D1-Hello/Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
     public static void main(String[] args) {
         System.out.println("你好，光伏！");
+        System.out.println("我是霍健卓。");
         System.out.println("我是霍健卓，这是我的第一个 Java 程序。");
     }
 }
