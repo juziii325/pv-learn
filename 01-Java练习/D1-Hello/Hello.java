947ab542c1a023e06e8f4128dddf0935c97915a0
霍健卓 <1243174864@qq.com>
2026-09-23 21:31:09 +0800

    队友改了第七行

--- 01-Java练习/D1-Hello/Hello.java
+++ 01-Java练习/D1-Hello/Hello.java
@@ -4,7 +4,7 @@ public class Hello
         System.out.println("你好，光伏！");
         System.out.println("我是霍健卓。");
         System.out.println("主干上改的这句话。");
-        System.out.println("这是我加的新行。");
+        System.out.println("队友改的第7行。");
     }
 }
 // 队友写的注释
