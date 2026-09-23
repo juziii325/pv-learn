cea88686cb4353ed63f010d9d74467e7929a884c
霍健卓 <1243174864@qq.com>
2026-09-24 01:21:48 +0800

    重命名练习文件夹为合法Java标识符，并加入VS Code运行配置

---
+++ JavaBasics/D2Var/VarDemo.java
@@ -0,0 +1,12 @@
+public class VarDemo {
+     public static void main(String[] args) {
+        int unitCount = 24;              // 组件数量
+        double capacity = 550.5;         // 单块容量（瓦）
+        boolean isFault = false;         // 是否故障
+        String deviceName = "1号逆变器";  // 设备名称
+        System.out.println(unitCount);
+        System.out.println(capacity);
+        System.out.println(isFault);
+        System.out.println(deviceName);
+      }
+}
