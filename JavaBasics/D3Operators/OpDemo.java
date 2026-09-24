718aca4bc32bfea6e6210ce4d4b8b87f5a9b5bce
霍健卓 <1243174864@qq.com>
2026-09-24 16:50:52 +0800

    第26课：算术运算符

---
+++ JavaBasics/D3Operators/OpDemo.java
@@ -0,0 +1,15 @@
+public class OpDemo {
+    public static void main(String[] args) {
+        int a=7;
+        int b=2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        double c=7.0;
+        System.out.println(c / b);
+        System.out.println("结果：" + a + b);
+        System.out.println("结果：" + (a + b));
+    }
+}
